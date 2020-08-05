c95cd9f70ac0a07700c0e4ee6ecd873cb683d09d
braguda <brian.aguda@gmail.com>
2020-08-05 14:34:17 -0700

    finished bio, contact and home pages

--- assets/index.js
+++ assets/index.js
@@ -1,27 +1,27 @@
-$(document).ready(() => {
-    const menuBtn = $('.menu-btn');
-    const hamburger = $('.menu-btn__burger');
-    const nav= $('.nav');
-    const menuNav = $('.menu-nav');
-    const navItems = document.querySelectorAll('.menu-nav__item');
-    let showMenu = false;
+// $(document).ready(() => {
+//     const menuBtn = $('.menu-btn');
+//     const hamburger = $('.menu-btn__burger');
+//     const nav= $('.nav');
+//     const menuNav = $('.menu-nav');
+//     const navItems = document.querySelectorAll('.menu-nav__item');
+//     let showMenu = false;
 
-    menuBtn.on('click', toggleMenu);
+//     menuBtn.on('click', toggleMenu);
 
-    function toggleMenu() {
-        if(!showMenu) {
-            hamburger.addClass('open');
-            nav.addClass('open');
-            menuNav.addClass('open');
-            navItems.forEach(item => item.classList.add('open'));
-            showMenu = true;
-        } else{
-            hamburger.removeClass('open');
-            nav.removeClass('open');
-            menuNav.removeClass('open');
-            navItems.forEach(item => item.classList.remove('open'));
-            showMenu = false;
-        }
-    }
+//     function toggleMenu() {
+//         if(!showMenu) {
+//             hamburger.addClass('open');
+//             nav.addClass('open');
+//             menuNav.addClass('open');
+//             navItems.forEach(item => item.classList.add('open'));
+//             showMenu = true;
+//         } else{
+//             hamburger.removeClass('open');
+//             nav.removeClass('open');
+//             menuNav.removeClass('open');
+//             navItems.forEach(item => item.classList.remove('open'));
+//             showMenu = false;
+//         }
+//     }
 
-});
+// });
